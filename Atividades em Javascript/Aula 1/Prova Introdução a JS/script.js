7f35014ca94129264c5bfcd69ddd1092efbd408d
Mateus Valentim <mateusvalentimdev@gmail.com>
2024-05-29 18:33:00 -0300

    Adicionei a proja introdutoria

---
+++ Atividades em Javascript/Aula 1/Prova Introdução a JS/script.js
@@ -0,0 +1,3 @@
+// Estranhamente na questãoo não estava pedindo para ter um input das notas ;-;
+var notas = [10, 9, 8];
+console.log(`Média das notas ${((notas[1]*1) + (notas[2]*2) + (notas[3]*3))/6}`);
